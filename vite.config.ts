679e2c50a44f34d10411558b5fe3c9ede6eb1b06
Andre Chicu <andriesh.chicu@gmail.com>
2026-03-08 08:01:12 +0200

    minify vite extension bundles

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     emptyOutDir: false,
-    minify: false,
+    minify: "esbuild",
+    reportCompressedSize: false,
     target: "firefox115"
+  },
+  esbuild: {
+    legalComments: "none"
   }
 });
